e9db40dc673f28ab580d722c205f330b7b2fe858
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-14 18:16:12 +0530

    correction

--- pylamp/src/app/components/text_img_compo.js
+++ pylamp/src/app/components/text_img_compo.js
@@ -1,12 +1,11 @@
-import React from 'react';
 
 export default function TextImgCompo({ imgSrc, imgAlt, title, desc, imgPosition }) {
     return (
         <div className="flex justify-center p-5">
             <div className="xl:w-3/4 w-full flex flex-col lg:flex-row justify-center lg:justify-between">
                 {imgPosition === 'left' && (
-                    <div className="md:w-1/2 self-center flex justify-center">
-                        <img src={imgSrc} alt={imgAlt} className="h-max-96" />
+                    <div className="md:w-1/2 self-center flex">
+                        <img src={imgSrc} alt={imgAlt} className="lg:h-80 sm:h-60 h-40" />
                     </div>
                 )}
                 <div className="lg:w-1/2 self-center p-5">
@@ -14,8 +13,8 @@ export default function TextImgCompo({ imgSrc, imgAlt, title, desc, imgPosition 
                     <p className="md:text-xl text-lg font-normal text-left">{desc}</p>
                 </div>
                 {imgPosition === 'right' && (
-                    <div className="md:w-1/2 self-center flex justify-center">
-                        <img src={imgSrc} alt={imgAlt} className="h-max-96" />
+                    <div className="md:w-1/2 self-center flex flex-row-reverse">
+                        <img src={imgSrc} alt={imgAlt} className="lg:h-80 sm:h-60 h-40" />
                     </div>
                 )}
             </div>
