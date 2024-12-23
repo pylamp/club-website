695c218bc05dda1299192509c3707f53e309aeaa
Sudarsan M <sudarsan1307@gmail.com>
2024-12-23 18:19:12 +0530

    error resolved

--- pylamp/src/app/components/Responsivegrid.js
+++ pylamp/src/app/components/Responsivegrid.js
@@ -7,6 +7,10 @@ const ResponsiveGrid = () => {
     { id: 2, name: "Event Name 2", imgSrc: "/upcoming/poster1.png" },
     { id: 3, name: "Event Name 3", imgSrc: "/upcoming/poster1.png" },
     { id: 4, name: "Event Name 4", imgSrc: "/upcoming/poster1.png" },
+    { id: 5, name: "Event Name 5", imgSrc: "/upcoming/poster1.png" },
+    { id: 6, name: "Event Name 6", imgSrc: "/upcoming/poster1.png" },
+    { id: 7, name: "Event Name 7", imgSrc: "/upcoming/poster1.png" },
+    { id: 8, name: "Event Name 8", imgSrc: "/upcoming/poster1.png" },
   ];
 
   return (
