b5a5768b9e5b1aeb184013c3f1407bbb82fa5c47
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-23 21:30:30 +0530

    grid  correction

--- pylamp/src/app/components/Responsivegrid.js
+++ pylamp/src/app/components/Responsivegrid.js
@@ -1,25 +1,11 @@
 // ResponsiveGrid.js
 import Image from "next/image";
 
-const ResponsiveGrid = () => {
-  const events = [
-    { id: 1, name: "Event Name 1", imgSrc: "/upcoming/poster1.png" },
-    { id: 2, name: "Event Name 2", imgSrc: "/upcoming/poster1.png" },
-    { id: 3, name: "Event Name 3", imgSrc: "/upcoming/poster1.png" },
-    { id: 4, name: "Event Name 4", imgSrc: "/upcoming/poster1.png" },
-    { id: 5, name: "Event Name 5", imgSrc: "/upcoming/poster1.png" },
-    { id: 6, name: "Event Name 6", imgSrc: "/upcoming/poster1.png" },
-    { id: 7, name: "Event Name 7", imgSrc: "/upcoming/poster1.png" },
-    { id: 8, name: "Event Name 8", imgSrc: "/upcoming/poster1.png" },
-  ];
-
+const ResponsiveGrid = ({ events }) => {
   return (
-    <div className=" min-h-screen py-8 flex flex-col justify-between">
-      <div className="container mx-auto px-4">
-        <h1 className="sm:text-6xl text-3xl font-bold text-center mb-6">
-          Events of 2024-25
-        </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+    <div className="flex flex-col w-full justify-center">
+      <div className="xl:w-3/4 w-full self-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:p-10 p-5">
           {events.map((event) => (
             <div key={event.id} className="w-full max-w-[636px] mx-auto">
               <div className="relative aspect-square">
@@ -38,7 +24,6 @@ const ResponsiveGrid = () => {
           ))}
         </div>
       </div>
-     
     </div>
   );
 };
--- pylamp/src/app/events/page.js
+++ pylamp/src/app/events/page.js
@@ -2,9 +2,22 @@ import ResponsiveGrid from "../components/Responsivegrid";
 import React from "react";
 
 export default function Home() {
+  const events = [
+    { id: 1, name: "Event Name 1", imgSrc: "/upcoming/poster1.png" },
+    { id: 2, name: "Event Name 2", imgSrc: "/upcoming/poster1.png" },
+    { id: 3, name: "Event Name 3", imgSrc: "/upcoming/poster1.png" },
+    { id: 4, name: "Event Name 4", imgSrc: "/upcoming/poster1.png" },
+    { id: 5, name: "Event Name 5", imgSrc: "/upcoming/poster1.png" },
+    { id: 6, name: "Event Name 6", imgSrc: "/upcoming/poster1.png" },
+    { id: 7, name: "Event Name 7", imgSrc: "/upcoming/poster1.png" },
+    { id: 8, name: "Event Name 8", imgSrc: "/upcoming/poster1.png" },
+  ];
   return (
     <div>
-      <ResponsiveGrid />
+      <h1 className="md:text-6xl sm:text-3xl text-lg font-bold text-center mb-6">
+          Events of 2024-25
+        </h1>
+      <ResponsiveGrid events={events} />
     </div>
   );
 }
